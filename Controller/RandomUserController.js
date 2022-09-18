3e0edfd7ed6c5b78654ecfa2962940830feac168
Mohammad Jahid <mohammadjahid0007@gmail.com>
2022-09-18 21:56:34 +0600

    got all users

--- Controller/RandomUserController.js
+++ Controller/RandomUserController.js
@@ -18,3 +18,19 @@ exports.getRandomUser = (req, res, next) => {
         }
     });
 }
+exports.getAllUser = (req, res, next) => {
+    fs.readFile("user.json", (err, data) => {
+        if (err) {
+            res.status(500).json({ success: false, message: err.message });
+            return;
+        }
+        try {
+            const { limit } = req.query;
+            const users = JSON.parse(data);
+            res.status(200).json({ success: true, data: users.slice(0, limit) });
+        } catch (err) {
+            next(err)
+            console.log("Error parsing JSON string:", err);
+        }
+    });
+}
--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { getRandomUser } = require("../Controller/RandomUserController");
+const { getRandomUser, getAllUser, getLimitedUser } = require("../Controller/RandomUserController");
 const router = express.Router();
 
 router.route("/random")
     .get(getRandomUser)
+
+router.get("/all", getAllUser)
+
+
 
 module.exports = router;
